12dfbd2434fe99aa50d7cce80014c3a064cf7733
Ricardo Martínez <112503383+RicardoxDev@users.noreply.github.com>
2023-01-12 19:49:49 -0400

    :turtle: different implementation of age

--- age.js
+++ age.js
@@ -1,4 +1,24 @@
 function age() {
+  const inputDay = document.getElementById("Date").value
+  const inputMonth = document.getElementById("Month").value
+  const inputYear = document.getElementById("Year").value
+
+  const inputDate = new Date(inputYear + "/" + inputMonth + "/" + inputDay)
+  const currentDate = new Date()
+  let finalDate = currentDate - inputDate
+  
+  const dayMS = 3600 * 24 * 1000
+  const yearMS = dayMS * 365
+  
+  const years = Math.floor(finalDate / yearMS)
+  const days = Math.floor((finalDate % yearMS) / dayMS)
+  const hours = Math.floor((((finalDate % yearMS) / dayMS) - days) * 24)
+
+  document.getElementById("age").innerHTML = `Tu edad es ${years} años ${days} dias y ${hours} horas`
+}
+
+/* before version
+function age() {
     let D1 = document.getElementById("Date").value;
     let M1 = document.getElementById("Month").value;
     let Y1 = document.getElementById("Year").value;
@@ -20,3 +40,4 @@ function age() {
     let Y = Y2 - Y1;
     document.getElementById("age").innerHTML = "Tu edad es " + Y + " años, " + M + " meses y " + D + " días ";
 }
+*/
